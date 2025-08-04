3c012420585c19c560dd4413bcb21c42af9f7689
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2025-08-04 19:20:22 +0200

    CI

--- src/common/assert.cpp
+++ src/common/assert.cpp
@@ -4,7 +4,13 @@
 #include "assert.h"
 #include "logging.h"
 
+#if defined(__GNUC__) || defined(__clang__)
 #define Crash() __asm__ __volatile__("int $3")
+#elif defined(_MSC_VER)
+#define Crash() __debugbreak()
+#else
+    #error Unsupported compiler
+#endif
 
 void assert_fail_impl() {
     std::fflush(stdout);
